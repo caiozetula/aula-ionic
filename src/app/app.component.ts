614365dc17fa37d2d886fdd783286ba232db40a7
caiozetula <caiozetula2@gmail.com>
2021-09-21 03:19:36 -0300

    criação das páginas alert, action sheet e badge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,9 @@ export class AppComponent {
   public appPages = [
     { title: 'Navegação', url: 'navegacao', icon: 'albums' },
     { title: 'Botões', url: 'botao', icon: 'construct' },
+    { title: 'Alert', url: 'alert', icon: 'alert-circle' },
+    { title: 'Action Sheet', url: 'actionsheet', icon: 'flash' },
+    { title: 'Badge', url: 'badge', icon: 'notifications' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
